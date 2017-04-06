fe608d10fc1a3817f6de5cd22174ee74acb2afd2
Pauline Whang <paulinewhang@Paulines-MacBook-Pro.local>
2017-04-06 00:06:59 -0400

    my first commit

---
+++ assets/javascript/train.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+	var auth = {
+		apiKey: 'AIzaSyDouPjKke2-rxbN74D4kZHE0zS2zQtlAAM',
+		authDomain: 'train-project-198a9.firebaseapp.com',
+		databaseURL: 'https://train-project-198a9.firebaseio.com/',
+		projectId: 'train-project-198a9',
+		storageBucket: 'train-project-198a9.appspot.com',
+		messagingSenderId: '1022109572490'
+	};
+
+
+})
